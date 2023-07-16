5d7135f41b2383642e05408349d16e87f3c9f5ae
rainoffallingstar <15860147513@163.com>
2023-07-16 14:05:43 +0800

    upload

--- app.R
+++ app.R
@@ -4,6 +4,11 @@ library(readxl)
 library(survival)
 library(survminer)
 
+# 定义需要用到的各种参数
+
+palette_list <- c("grey","npg","aaas","lancet","jco", 
+"ucscgb","uchicago","simpsons","rickandmorty","hue")
+
 # 定义用户界面
 ui <- fluidPage(
   titlePanel("生存分析"),
@@ -15,12 +20,13 @@ ui <- fluidPage(
       textInput("status", "输入结局变量名","status"),
       textInput("time", "输入结局变量名","time"),
       uiOutput("varSelect"),
+      selectInput("palettes","选择调色板",choices = palette_list,"hue" )
       actionButton("go", "开始分析")
     ),
     mainPanel(
       tableOutput("table"),
-      plotOutput("plot")
-      #plotOutput("plot2")
+      plotOutput("plot"),
+      plotOutput("plot2")
     )
   )
 )
@@ -46,13 +52,42 @@ server <- function(input, output, session) {
     })
     
     output$plot <- renderPlot({
-      fit <- sur_fit()
-      ggsurvplot(fit, data = data(), risk.table = TRUE)
+      ggsurvplot(sur_fit(), data = data(), 
+                    risk.table = TRUE,
+                    surv.median.line = "hv",# 增加中位生存时间
+                    conf.int = TRUE ,# 增加置信区间
+                    pval = TRUE, # 添加P值
+                    add.all = TRUE , # 添加总患者生存曲线
+                    palette = input$palettes,  # 自定义调色板
+                    xlab = "Follow up time(d)", # 指定x轴标签
+                    ylab = "Survival probability",# 指定x轴标签
+                    legend = c(0.8,0.75), # 指定图例位置
+                    legend.title = "", # 设置图例标题
+                    legend.labs = factor(input$variable), # 指定图例分组标签
+                    break.x.by = 100 # 设置x轴刻度间距
+                    )
     })
   })
-  #output$plot2 <- renderPlot({
+  output$plot2 <- renderPlot({
+    ggsurvplot(sur_fit(), data = data(), 
+           conf.int = TRUE, # 增加置信区间
+           fun = "cumhaz", # 绘制累计风险曲线
+           cumevents = TRUE,
+           surv.median.line = "hv",# 增加中位生存时间
+           conf.int = TRUE ,# 增加置信区间
+           pval = TRUE, # 添加P值
+           add.all = TRUE , # 添加总患者生存曲线
+           palette = input$palettes,  # 自定义调色板
+           xlab = "Follow up time(d)", # 指定x轴标签
+           ylab = "Survival probability",# 指定x轴标签
+           legend = c(0.8,0.75), # 指定图例位置
+           legend.title = "", # 设置图例标题
+           legend.labs = factor(input$variable), # 指定图例分组标签
+           break.x.by = 100# 设置x轴刻度间距
+           )
 
-  #})
+
+  })
 }
 
 # 运行应用
