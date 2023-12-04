3725647b79a580b8a3e4c5c074aa46ea419d6cfc
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2023-12-04 16:56:14 +0800

    up2date

--- testcode.R
+++ testcode.R
@@ -1,17 +1,15 @@
-data <- read_excel("1.xlsx")
-datas <- data %>% mutate(
-  times = time,
-  status = status,
-  variable = sex
-) %>% 
-  select(all_of(c("times","status","variable"))) %>% 
-  na.omit()
 
-fit <- survfit(Surv(time, status) ~ sex, 
-               data = data)
+library(readxl)
+library(survival)
+library(survminer)
+library(stringr)
+library(dplyr)
+library(tidyr)
+fit <- survfit(Surv(time, status) ~ cd97inblast, 
+               data = survival)
 fit
 summary(fit)
-ggsurvplot(fit, data = data, 
+ggsurvplot(fit, data = survival, 
            risk.table = TRUE,
            #surv.median.line = "hv",# 增加中位生存时间
            conf.int = TRUE ,# 增加置信区间
@@ -25,15 +23,3 @@ ggsurvplot(fit, data = data,
            #legend.labs = 3, # 指定图例分组标签
            #break.x.by = 100 # 设置x轴刻度间距
 )
-ggsurvplot(fit, data = data
-)
-
-ggsurvplot(fit, data = datas, 
-           conf.int = TRUE, # 增加置信区间
-           fun = "cumhaz")
-
-datas <- within(datas,
-                status = factor(datas$status,levels = c(1,2),
-                
-                variable = factor(datas$variable,levels = c(1,2))
-                
